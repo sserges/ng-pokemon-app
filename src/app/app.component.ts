623ea870ac2f2cb3b2706ea6778da56729af1312
sserges <agounkeserge@gmail.com>
2019-11-20 16:46:54 +0000

    Declare a template with the templateUrl attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,7 @@ import { POKEMONS } from "./mock-pokemons";
 
 @Component({
   selector: "pokemon-app",
-  template: `
-    <h1>Liste de pokémons</h1>
-  `
+  templateUrl: `./app/app.component.html`
 })
 export class AppComponent implements OnInit {
 
--- src/dist/app/app.component.js
+++ src/dist/app/app.component.js
@@ -7,14 +7,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var mock_pokemons_1 = require("./mock-pokemons");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.name = "Angular";
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.pokemons = mock_pokemons_1.POKEMONS;
+    };
+    AppComponent.prototype.selectPokemon = function (pokemon) {
+        alert("Vous avez cliqué sur " + pokemon.name);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: "pokemon-app",
-            template: "\n    <h1>Hello {{ name }}</h1>\n  "
+            templateUrl: "./app/app.component.html"
         })
     ], AppComponent);
     return AppComponent;
