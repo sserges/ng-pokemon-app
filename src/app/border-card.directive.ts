ef7781a746cecffc250112a63ddc3aa7b621b07b
sserges <agounkeserge@gmail.com>
2019-11-25 15:18:40 +0000

    Add a parameter to our directive

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]' 
@@ -7,10 +7,12 @@ export class BorderCardDirective {
 	constructor(private el: ElementRef) {
 		this.setBorder('#f5f5f5');
 		this.setHeight(180);
-    }
+	}
+	
+	@Input('pkmnBorderCard') borderColor: string;
     
     @HostListener('mouseenter') onMouseEnter() {
-        this.setBorder('#009688');
+        this.setBorder(this.borderColor || '#009688');
     }
 
     @HostListener('mouseleave') onMouseLeave() {
