b516121ef7a37304c2e9fe9521899acacb8bfc40
sserges <agounkeserge@gmail.com>
2019-11-28 20:19:44 +0000

    Set up the roads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 import { Pokemon } from "./pokemon";
 import { POKEMONS } from "./mock-pokemons";
-import { R3TargetBinder } from "@angular/compiler";
+
 
 @Component({
   selector: "pokemon-app",
   templateUrl: `./app/app.component.html`
 })
-export class AppComponent implements OnInit {
-
-  private pokemons: Pokemon[];
-  private title: string = "Liste des pokémons";
-
-
-  ngOnInit() {
-    this.pokemons = POKEMONS;
-  }
-
-  selectPokemon(pokemon: Pokemon) {
-    alert("Vous avez cliqué sur " + pokemon.name);
-  }
+export class AppComponent {
 }
